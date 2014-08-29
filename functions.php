73a64c9cefd75ffeccc6c8a0017c538fece2918d
ryelle <kelly.dwan@gmail.com>
2014-08-29 12:51:42 -0400

    Remove writr’s color scheme setting and add a new one for the child theme schemes, so that the parent theme sanitization doesn’t blow away the child schemes.

--- functions.php
+++ functions.php
@@ -4,11 +4,18 @@
  */
 function writr_child_scripts() {
 
-	$colorscheme = get_theme_mod( 'writr_color_scheme' );
+	$colorscheme = get_theme_mod( 'writr_child_color_scheme' );
+	if ( 'default' == $colorscheme ) {
+		return;
+	}
+
 	$child_choices = writr_child_color_options();
 	if ( $colorscheme && in_array( $colorscheme, array_keys( $child_choices ) ) ) {
 		wp_deregister_style( 'writr-color-scheme' );
 		wp_enqueue_style( 'writr-color-scheme', get_stylesheet_directory_uri() . '/css/' . $colorscheme . '.css' , array(), null );
+	} else {
+		wp_deregister_style( 'writr-color-scheme' );
+		wp_enqueue_style( 'writr-color-scheme', get_template_directory_uri() . '/css/' . $colorscheme . '.css' , array(), null );
 	}
 
 }
@@ -30,6 +37,22 @@ function writr_child_color_options(){
 }
 
 /**
+ * Default Color Schemes for Writr
+ *
+ * @return  array  Array of color schemes in the format of filename => label.
+ */
+function writr_child_default_color_options(){
+	return array(
+		'default'       => __( 'Turquoise', 'writr' ),
+		'blue'          => __( 'Blue', 'writr' ),
+		'green'         => __( 'Green', 'writr' ),
+		'grey'          => __( 'Grey', 'writr' ),
+		'purple'        => __( 'Purple', 'writr' ),
+		'red'           => __( 'Red', 'writr' ),
+	);
+}
+
+/**
  * Add options to the Theme Customizer.
  *
  * We overwrite the control for color schemes by running this action later than the
@@ -39,19 +62,19 @@ function writr_child_color_options(){
  */
 function writr_child_customize_register( $wp_customize ) {
 
-	// Default choices from Writr
-	$choices = array(
-		'default'       => __( 'Turquoise', 'writr' ),
-		'blue'          => __( 'Blue', 'writr' ),
-		'green'         => __( 'Green', 'writr' ),
-		'grey'          => __( 'Grey', 'writr' ),
-		'purple'        => __( 'Purple', 'writr' ),
-		'red'           => __( 'Red', 'writr' ),
-	);
+	$choices = array_merge( writr_child_default_color_options(), writr_child_color_options() );
 
-	$choices = array_merge( $choices, writr_child_color_options() );
+	$wp_customize->remove_setting( 'writr_color_scheme' );
+	$wp_customize->remove_control( 'writr_color_scheme' );
 
-	$wp_customize->add_control( 'writr_color_scheme', array(
+	$wp_customize->add_setting( 'writr_child_color_scheme', array(
+		'default'		    => 'default',
+		'type'			    => 'theme_mod',
+		'capability'	    => 'edit_theme_options',
+		'sanitize_callback' => 'writr_child_sanitize_color_scheme',
+	) );
+
+	$wp_customize->add_control( 'writr_child_color_scheme', array(
 		'label'    => 'Color Scheme',
 		'section'  => 'writr_theme_options',
 		'type'     => 'select',
@@ -61,3 +84,23 @@ function writr_child_customize_register( $wp_customize ) {
 
 }
 add_action( 'customize_register', 'writr_child_customize_register', 15 );
+
+/**
+ * Sanitize the Color Scheme value.
+ *
+ * @param string $color
+ * @return string
+ */
+function writr_child_sanitize_color_scheme( $color ) {
+
+	$default_colors = writr_child_default_color_options();
+	$added_colors = writr_child_color_options();
+
+	$choices = array_merge( $default_colors, $added_colors );
+	$choices = array_keys( $choices );
+
+	if ( ! in_array( $color, $choices ) ) {
+		$color = 'default';
+	}
+	return $color;
+}
